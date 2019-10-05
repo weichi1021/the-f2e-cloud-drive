65e25bead38d37560897e8de44160e8036c3b348
vicky.wen <vicky.wen@drsays.org>
2019-10-05 14:49:06 +0800

    meta, filter

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .container(@click="hideContextMenu")
+  .container(v-loading="isLoading", @click="hideContextMenu")
     el-container
       el-header
         .title Cloud Drive
@@ -7,7 +7,7 @@
           el-col
             el-input(prefix-icon="el-icon-search", placeholder="Search Drive", v-model="search")
           el-col.text-right(:span="6")
-            el-avatar {{ avatarDisplayName }}
+            el-avatar User
       el-container
         el-aside(width="185px")
           ul.aside-menu
@@ -16,7 +16,7 @@
                 el-button(type="default", icon="el-icon-plus") New
                 el-dropdown-menu(slot="dropdown")
                   el-dropdown-item
-                    el-upload(:multiple="false", action="", :show-file-list="false" :on-success="fileUploadStorageHandler") File upload
+                    el-upload(:multiple="false", action="", :show-file-list="false", :before-upload="beforeUpload", :on-success="uploadFileHandler") File upload
             li(:class="{active: menuActive == 'My Drive'}", @click="menuActiveHandler('My Drive')")
               i.el-icon-files
               span My Drive
@@ -29,26 +29,39 @@
             li(:class="{active: menuActive == 'Trash'}", @click="menuActiveHandler('Trash')")
               i.el-icon-delete
               span Trash
+            el-divider
+            li.list-btn
+              i.el-icon-box
+              span Storage
+              el-progress(:percentage="storageSizePercent" :stroke-width="3" :show-text="false")
+              span.small-text {{ storageDisplayName }}
         el-main
           .main-title {{ menuActive }}
-          el-table(:data="mainData", style="width: 100%", oncontextmenu="return false;", @row-contextmenu="showContextMenu")
-            el-table-column(prop="display_name", label="Name", :sortable="true")
+          el-table(:data="filterMainData", style="width: 100%", oncontextmenu="return false;", unselectable="on", @row-contextmenu="showContextMenu")
+            el-table-column(prop="display_name", label="Name", :sortable="(menuActive == 'My Drive') ")
               template(slot-scope="scope")
                 i(:class="displayIcon(scope.row.type)")
-                span &nbsp;&nbsp;{{ scope.row.display_name }}
+                span &nbsp;&nbsp;{{ scope.row.display_name }}&nbsp;
+                i.el-icon-star-on(v-if="scope.row.is_starred && (menuActive !== 'Starred') && ((menuActive !== 'Trash'))")
             el-table-column(prop="size_display_name", label="File size", width="120")
             el-table-column(prop="type", label="File type", width="140")
-            el-table-column(prop="last_modify_time", label="Last modified", :sortable="true", width="140")
+            el-table-column(prop="last_modify_time", label="Last modified", :sortable="(menuActive == 'My Drive')", width="140")
           ul#context-menu
-            li
+            li(@click="previewFileHandler", v-if="menuActive !== 'Trash'")
+              i.el-icon-view
+              span &nbsp;&nbsp;Preview
+            li(@click="starredFileHandler", v-if="menuActive !== 'Trash'")
               i.el-icon-star-off
-              span &nbsp;&nbsp;Add to Starred
-            li(@click="downloadFileHandler")
-              i.el-icon-download
-              span &nbsp;&nbsp;Download
-            li(@click="removeFileHandler")
+              span &nbsp;&nbsp;{{ starredDisplayName }}
+            li(@click="activeFileHandler(false)", v-if="menuActive !== 'Trash'")
               i.el-icon-delete
               span &nbsp;&nbsp;Remove
+            li(@click="activeFileHandler(true)", v-if="menuActive == 'Trash'")
+              i.el-icon-refresh-left
+              span &nbsp;&nbsp;Restore
+            li(@click="removeFileHandler", v-if="menuActive == 'Trash'")
+              i.el-icon-delete
+              span &nbsp;&nbsp;Remove Forever
 </template>
 
 <script>
@@ -61,19 +74,19 @@
   export default {
     head() {
       return {
-        title: 'My Drive',
+        title: this.menuActive,
         titleTemplate: '%s | Cloud Drive'
       }
     },
     data() {
       return {
+        isLoading: true,
         search: '',
         menuActive: 'My Drive',
         userProfile: {},
-        mainData: [
-          // {"id":"1","display_name":"1day.pdf","is_active":true,"last_modify_time":"2019/9/4","path":"Test/1day.pdf","size":"220.74 KB","type":"application/pdf"}, {"id":"2","display_name":"2day.pdf","is_active":true,"last_modify_time":"2019/9/2","path":"Test/2day.pdf","size":"220.74 KB","type":"application/pdf"}, {"id":"3","display_name":"3day.pdf","is_active":true,"last_modify_time":"2019/9/10","path":"Test/3day.pdf","size":"220.74 KB","type":"application/pdf"}
-        ],
+        mainData: [],
         clickedData: {},
+        totalSize: 0,
       }
     },
     created() {
@@ -85,25 +98,74 @@
         const { display_name } = this.userProfile;
         let avatarName = display_name || 'A'
         return avatarName.substr(0, 1).toLocaleUpperCase()
-      }
+      },
+      storageDisplayName() {
+        const is1MB = 1024*1024;
+        return `${this.formatByte(this.totalSize)} of ${this.formatByte(is1MB)} used`
+      },
+      storageSizePercent() {
+        const is1MB = 1024*1024;
+        return Math.floor(this.totalSize / is1MB * 100)
+      },
+      starredDisplayName() {
+        return (!this.clickedData.is_starred)? `Add to Starred` : `Remove from Starred`
+      },
+      filterMainData() {
+        return this.mainData.filter(item => {
+            const searchLowerCase = this.search.toLocaleLowerCase();
+            const isSearch = (item.display_name.toLocaleLowerCase().indexOf(searchLowerCase) !== -1) || (!searchLowerCase)
+            const isMyDrive = (this.menuActive == 'My Drive' && item.is_active && isSearch)
+            const isStarred = (this.menuActive == 'Starred' && item.is_active && item.is_starred && isSearch)
+            const isRecent = (this.menuActive == 'Recent' && item.is_active && isSearch)
+            const isTrash = (this.menuActive == 'Trash' && !item.is_active && isSearch)
+            return isMyDrive || isStarred || isRecent || isTrash
+          }).sort((a, b) => {
+            const isRecent = (this.menuActive == 'Recent')
+            if(isRecent){
+              const nameA = a.last_modify_time.toUpperCase(); // ignore upper and lowercase
+              const nameB = b.last_modify_time.toUpperCase(); // ignore upper and lowercase
+              if (nameA < nameB) {
+                return 1;
+              }
+              if (nameA > nameB) {
+                return -1;
+              }
+            }
+
+            // names must be equal
+            return 0;
+        });
+      },
     },
     methods: {
       // handler
       menuActiveHandler(name) {
         this.menuActive = name;
       },
-      async fileUploadStorageHandler(response, file) {
+      // upload
+      beforeUpload(file) {
+        const isLt1M = ((this.totalSize + file.size) / 1024 / 1024) < 1;
+        return isLt1M;
+      },
+      async uploadFileHandler(response, file){
+        await this.uploadFileStorage(file)
+        this.uploadFileDB(file.raw)
+        this.menuActiveHandler('My Drive')
+      },
+      async uploadFileStorage(file) {
         try{
           console.log(file)
           var mountainsRef = storageRef.child(`${this.userProfile.display_name}/${file.name}`);
           await mountainsRef.put(file.raw, {contentType: file.type})
-          this.fileUploadDBHandler(file.raw)
+          this.getFolder()
         }catch(err){
           console.log(err);
         }
       },
-      fileUploadDBHandler(file) {
+      async uploadFileDB(file) {
         try{
+          const path = `${this.userProfile.display_name}/${file.name}`;
+          const url = await storageRef.child(path).getDownloadURL();
           const data = {
             display_name: file.name,
             type: file.type,
@@ -111,42 +173,75 @@
             size_display_name: this.formatByte(file.size),
             is_active: true,
             is_starred: false,
-            path: `${this.userProfile.display_name}/${file.name}`,
+            path: path,
+            file_url: url,
             last_modify_time: this.getTodayDate()
           };
           const resp = filesRef.doc().set(data)
           this.mainData.push(data)
+          this.getTotalSize()
         }catch(err){
           console.log(err);
         }
       },
+      // remove
       removeFileHandler() {
+        this.removeFileStorage();
+        this.removeFileDB();
+      },
+      removeFileStorage() {
         try{
           var desertRef = storageRef.child(this.clickedData.path);
           desertRef.delete()
         }catch(err){
           console.log(err)
         }
+      },
+      removeFileDB() {
         try{
           filesRef.doc(this.clickedData.id).delete();
+          const findIndex = this.mainData.findIndex(item => item.id == this.clickedData.id)
+          if(findIndex !== undefined){
+            this.mainData.splice(findIndex, 1)
+            this.getTotalSize();
+          }
         }catch(err){
           console.log(err)
         }
       },
-      async downloadFileHandler() {
+      activeFileHandler(active) {
         try{
-          const url = await storageRef.child(this.clickedData.path).getDownloadURL();
-          console.log(url)
-          var xhr = new XMLHttpRequest();
-          xhr.responseType = 'blob';
-          xhr.onload = function(event) {
-            var blob = xhr.response;
-          };
-          xhr.open('GET', url);
-          xhr.send();
-          // setTimeout(()=>{
-          //   window.open(xhr.responseURL, '_blank')
-          // }, 1000)
+          console.log(this.clickedData.id)
+          filesRef.doc(this.clickedData.id).update({ is_active: active });
+          const findIndex = this.mainData.findIndex(item => item.id == this.clickedData.id)
+          if(findIndex !== undefined){
+            this.mainData[findIndex].is_active = active
+          }
+        }catch(err){
+          console.log(err)
+        }
+      },
+      // preview
+      async previewFileHandler() {
+        if(!!this.clickedData.file_url){
+          window.open(this.clickedData.file_url, '_blank')
+        }else{
+          try{
+            const url = await storageRef.child(this.clickedData.path).getDownloadURL();
+            window.open(url, '_blank')
+          }catch(err){
+            console.log(err)
+          }
+        }
+      },
+      // starred
+      starredFileHandler() {
+        try{
+          filesRef.doc(this.clickedData.id).update({ is_starred: !this.clickedData.is_starred });
+          const findIndex = this.mainData.findIndex(item => item.id == this.clickedData.id)
+          if(findIndex !== undefined){
+            this.mainData[findIndex].is_starred = !this.mainData[findIndex].is_starred
+          }
         }catch(err){
           console.log(err)
         }
@@ -166,11 +261,16 @@
         try{
           const querySnapshot = await filesRef.get()
           this.mainData = querySnapshot.docs.map(doc => { return { id: doc.id, ...doc.data() } });
+          this.getTotalSize();
+          this.isLoading = false;
           console.log('get folder = %o', this.mainData)
         }catch(err){
           console.log(err);
         }
-      }, 
+      },
+      getTotalSize() {
+        this.totalSize = this.mainData.reduce((accumulator, currentItem) => { return accumulator + currentItem.size }, 0)
+      },
       // context menu
       showContextMenu(row, column, event) {
         console.log(row, column, event)
@@ -197,8 +297,8 @@
         document.getElementById("context-menu").classList.add('active');
       },
       hideContextMenu() {
-        console.log('hide')
-        document.getElementById("context-menu").classList.remove('active');
+        document.getElementById('context-menu').classList.remove('active');
+        Object.assign(this.$data.clickedData, this.$options.data.clickedData)
       },
       // get function
       displayIcon(type) {
@@ -257,7 +357,7 @@
     color: #606266
   .el-header
     height: 70px !important
-    padding: 15px 30px
+    padding: 15px 20px
     margin-top: 10px
     margin-bottom: 20px
     border-bottom: 1px solid #E4E7ED
@@ -265,7 +365,7 @@
     float: left
     font-size: 24px
     line-height: 40px
-    margin-right: 30px
+    margin-right: 60px
   ul.nav, ul.aside-menu, ul#context-menu
     list-style-type: none
     margin: 0px
@@ -276,13 +376,18 @@
       float: left
     li + li
       margin-left: 15px
-  ul.aside-menu li
-    cursor: default
-    padding: 10px 40px
-    i
-      margin-right: 8px
-  ul.aside-menu li.list-btn
-    padding: 0px 40px 10px
+  ul.aside-menu
+    li, li.list-btn
+      cursor: default
+      padding: 10px 20px
+      i
+        margin-right: 8px
+    .el-divider
+      margin: 8px 0px
+    .el-progress
+      margin: 12px 0px 5px
+    .small-text
+      font-size: 11px
   ul.aside-menu li:not(.list-btn):hover
     background-color: rgba(#409EFF, 0.2)
   ul.aside-menu li.active
@@ -291,10 +396,15 @@
     padding: 0px 20px
     .main-title
       font-size: 16px
-      padding-bottom: 15px
+      padding: 0px 10px 15px
       margin-bottom: 10px
       border-bottom: 1px solid #E4E7ED
+    .el-table
+      -webkit-user-select: none
+      -moz-user-select: none
   ul#context-menu
+    -webkit-user-select: none
+    -moz-user-select: none
     display: none;
     position: absolute
     padding: 5px 0px !important
